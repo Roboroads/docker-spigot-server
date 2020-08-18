6c7243d9ae0baba2a51c4f2a3eabbd51b3ca0336
Robbin Schepers <robbin@deschepers.nl>
2020-08-18 15:18:00 +0200

    Removes spigot jar before updating

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -29,6 +29,7 @@ case "$1" in
   update)
     mkdir -p /minecraft/buildtools
     cd /minecraft/buildtools
+    rm spigot*.jar
     curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
     java -jar BuildTools.jar --rev $SPIGOT_VERSION
     cp spigot*.jar ../spigot.jar
